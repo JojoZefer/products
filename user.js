96bf2ea64cf21437f722677cf2cc5d302880c390
JojoZefer <108221802+JojoZefer@users.noreply.github.com>
2023-03-03 19:21:52 +0200

    Update user.js

--- user.js
+++ user.js
@@ -2,6 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get('id');
 
 let profile = document.getElementById('profile');
+let productsGrid = document.getElementById('user-products-grid')
 let url = 'https://my-json-server.typicode.com/JojoZefer/products'
 
 let userRequest = new XMLHttpRequest();
@@ -15,7 +16,28 @@ userRequest.onload = function() {
      <h2>${user.sirname}</h2>
      <img class="profile-img" src="${user.photo_url}" alt="${user.name}">
      <p>Balance: ${user.balance}</p>
-	`
+	`;
 }
 
 userRequest.send();
+
+let productsRequest = new XMLHttpRequest();
+
+productsRequest.open('GET', `${url}/products?author_id=${id}`);
+productsRequest.responseType = 'json'
+productsRequest.onload = function() {
+	let products = productsRequest.response;
+	productsGrid.innerHTML = null;
+	products.forEach(p => {
+         productsGrid.innerHTML += `
+            <div class="product">
+                <h2 class="product-name">${p.name}</h2>
+                <img class="product-photo" src="${p.photo_url}" alt="${p.name}">
+                <p class="product-price"><b>Price: </b>${p.price}</p>
+                <p class="product-description"><b>Description:</b>${p.description}</p>
+            </div>
+         `;
+	});
+}
+
+productsRequest.send();
