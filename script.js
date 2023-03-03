9668bc43407c815f6117fb46fa9382a49ec29f55
JojoZefer <108221802+JojoZefer@users.noreply.github.com>
2023-03-03 18:52:31 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let productsGrid = document.getElementById('products-grid');
+let productsArray = [];
+let xhr = new XMLHttpRequest();
+let url = 'https://my-json-server.typicode.com/JojoZefer/products'
+
+xhr.open('GET', url + '/products');
+xhr.responseType = 'json';
+xhr.onload = function() {
+	let products = xhr.response;
+	productsGrid.innerHTML = null;
+	products.forEach(p => {
+		productsArray.push(p);
+		let pElem = document.createElement('div');
+		pElem.classList.add('products');
+		pElem.innerHTML = `
+         <h2 class = 'products-name'>${p.name}</h2>
+         <img class='product-photo' src="$${p.photo_url}" alt="${p.name}">
+         <p class='products-price'><b>Price:</b>${p.price}</p>
+         <p class='products-description'><b>Description</b>${p.description}</p>
+         <a href="profile.html?id=${p.author_id}">Seller profiles</a>
+         <button>Buy</button>
+		`;
+		productsGrid.append(pElem);
+	});
+}
+xhr.send();
