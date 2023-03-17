8fd3ee7ee23396d3a42e3d2c46c522a3b3a9f9e5
JojoZefer <108221802+JojoZefer@users.noreply.github.com>
2023-03-17 18:24:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let url = 'https://my-json-server.typicode.com/JojoZefer/products'
 xhr.open('GET', url + '/products');
 xhr.responseType = 'json';
 xhr.onload = function() {
-	let products = xhr.response;
+	products = xhr.response;
 	productsGrid.innerHTML = null;
 	products.forEach(p => {
 		productsArray.push(p);
@@ -15,10 +15,10 @@ xhr.onload = function() {
 		pElem.innerHTML = `
          <h2 class = 'products-name'>${p.name}</h2>
          <img class='product-photo' src="$${p.photo_url}" alt="${p.name}">
-         <p class='products-price'><b>Price:</b>${p.price}</p>
+         <p class='products-price'><b>Price:</b>${p.price}UAH</p>
          <p class='products-description'><b>Description</b>${p.description}</p>
          <a href="profile.html?id=${p.author_id}">Seller profiles</a>
-         <button>Buy</button>
+         <button onclick="addProductToCart(${p.id})">Buy</button>
 		`;
 		productsGrid.append(pElem);
 	});
@@ -34,6 +34,29 @@ function addProductToCart(id) {
 		return p.id == id;
 	})
 	cart.push(product);
+	drawCartProducts();
+}
+
+function drawCartProducts() {
+	if(cart.length == 0) return cartProd.innerHTML = 'Cart is empty'
+	cartProd.innerHTML = null;
+	let sun = 0;
+	cart.forEach(function(p){
+		cartProd.innerHTML +- `
+        <p><img src="${p.photo_url}" alt="${p.name}">${p.name} |${p.price}UAH</p>
+        <hr><br>
+		`;
+		sum += p.price;
+	});
+	cartProd.innerHTML += `
+       <p>Total price: ${sum}UAH</p>
+       <button onclick="buyAll()">Buy all</button>
+	`	
+}
+
+function buyAll() {
+	cart = [];
+	cartProd.innerHTML = 'Money was withdrawn from your credit card'
 }
 
 function openCart() {
